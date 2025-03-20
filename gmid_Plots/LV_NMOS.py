d9b25359913179c54c596ee313b9512b21e52613
Nithin P <123294639+chennakeshavadasa@users.noreply.github.com>
2025-03-20 19:15:12 +0530

    Update LV_NMOS.py

--- gmid_Plots/LV_NMOS.py
+++ gmid_Plots/LV_NMOS.py
@@ -3,7 +3,7 @@ from matplotlib.widgets import CheckButtons  # type: ignore
 import numpy as np
 
 # Define the path to the directory
-path = r"C:\Users\NITHIN P\Downloads\gmid_IHP130\gmid Data\LV_NMOS\\"
+path = r"C:\Users\NITHIN P\Downloads\gmid_IHP130\gmid Data\LV_NMOS\\" # Please provide your path file
 
 # Initialize lists for storing results
 vgs = [[] for _ in range(13)]
